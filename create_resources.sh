bcfa329f12936ed886085cb9e5d00fd009ad7dea
Tamal Saha <tamal@appscode.com>
2024-08-08 19:52:48 -0700

    fmt
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- create_resources.sh
+++ create_resources.sh
@@ -11,7 +11,6 @@ apt-get -y update
 apt upgrade -y
 apt-get install jq unzip -y >/dev/null
 
-
 #variables
 BUCKET_NAME=""
 PUBLIC_IP=""
@@ -19,44 +18,44 @@ RAND=""
 GOOGLE_APPLICATION_CREDENTIALS_STRING=$(cat $GOOGLE_APPLICATION_CREDENTIALS | base64 -w 0)
 
 install_gcloud() {
-    echo "Installing Google Cloud SDK..."
+  echo "Installing Google Cloud SDK..."
 
-    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    sudo apt-get install apt-transport-https ca-certificates gnupg -y
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  sudo apt-get install apt-transport-https ca-certificates gnupg -y
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 
-    sudo apt-get update && sudo apt-get install google-cloud-sdk -y >/dev/null
+  sudo apt-get update && sudo apt-get install google-cloud-sdk -y >/dev/null
 }
-setup_gcloud(){
 
-    gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
-    gcloud config set project "$GCP_PROJECT"
+setup_gcloud() {
+  gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
+  gcloud config set project "$GCP_PROJECT"
 }
 
 create_bucket() {
-    RAND=$(head /dev/urandom | tr -dc 'a-z' | head -c 4)
-    BUCKET_NAME="ace-bucket-$RAND"
-    install_gcloud
-    setup_gcloud
-    echo "Creating bucket: $BUCKET_NAME in project: $GCP_PROJECT and region: ${REGION}"
+  RAND=$(head /dev/urandom | tr -dc 'a-z' | head -c 4)
+  BUCKET_NAME="ace-bucket-$RAND"
+  install_gcloud
+  setup_gcloud
+  echo "Creating bucket: $BUCKET_NAME in project: $GCP_PROJECT and region: ${REGION}"
 
-    gsutil mb -p "$GCP_PROJECT" -c STANDARD -l "${REGION}" gs://"$BUCKET_NAME"/
-    if [ $? -eq 0 ]; then
-        echo "Bucket $BUCKET_NAME created successfully."
-    else
-        echo "Failed to create bucket $BUCKET_NAME."
-        exit
-    fi
+  gsutil mb -p "$GCP_PROJECT" -c STANDARD -l "${REGION}" gs://"$BUCKET_NAME"/
+  if [ $? -eq 0 ]; then
+    echo "Bucket $BUCKET_NAME created successfully."
+  else
+    echo "Failed to create bucket $BUCKET_NAME."
+    exit
+  fi
 }
 
 create_static_public_ip() {
-    PUBLIC_IP=$(gcloud compute addresses create "ace-ip-$RAND" --global --ip-version IPV4 --format="get(address)")
+  PUBLIC_IP=$(gcloud compute addresses create "ace-ip-$RAND" --global --ip-version IPV4 --format="get(address)")
 }
 
 webhook_api() {
-    resp=$(curl -X POST https://appscode.com/marketplace/api/v1/marketplaces/gcp/notification/resource?secret=72iuueq9sbiomgxgcbdbehfbhai1fqgg4dlpndxsh4rstoptvbvrkje88ob6cdkuv16nbpoym1/griswiujgga== \
-        -H "Content-Type: application/json" \
-        -d '{
+  resp=$(curl -X POST https://appscode.com/marketplace/api/v1/marketplaces/gcp/notification/resource?secret=72iuueq9sbiomgxgcbdbehfbhai1fqgg4dlpndxsh4rstoptvbvrkje88ob6cdkuv16nbpoym1/griswiujgga== \
+    -H "Content-Type: application/json" \
+    -d '{
               "eventType": "BIND",
               "bindingInfo": {
                 "installerID": "'${INSTALLER_ID}'",
@@ -98,20 +97,19 @@ webhook_api() {
               }
             }')
 
-    link=$(echo ${resp} | jq -r '.link')
-    if [ ${link} == "null" ]; then   exit ; fi
+  link=$(echo ${resp} | jq -r '.link')
+  if [ ${link} == "null" ]; then exit; fi
 
-    mkdir new
-    cd new
-    curl -L "${link}" -o "archive.zip"
-    unzip archive.zip >/dev/null
-    cd ..
+  mkdir new
+  cd new
+  curl -L "${link}" -o "archive.zip"
+  unzip archive.zip >/dev/null
+  cd ..
 }
 
-init(){
-    create_bucket
-    create_static_public_ip
-    webhook_api
+init() {
+  create_bucket
+  create_static_public_ip
+  webhook_api
 }
 init
-
